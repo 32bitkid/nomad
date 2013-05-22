51fe910194f9a18cbeba74b5e68dff6067fa2bf4
rajiv <rajiv.delwadia@versionone.com>
2013-05-21 23:35:02 -0400

    load waypoints, and serve ~/waypoint/<N> route

--- web.js
+++ web.js
@@ -1,12 +1,29 @@
-var restify = require('restify')
+var gpx = require('./util/gpx')
+var fs = require('fs')
+var xmlParser = require('xmldom').DOMParser
+
+var waypoints = []
+fs.readFile('./raw_data/ct_full.gpx', 'utf8', function (err, xml) {
+	var dom = new xmlParser().parseFromString(xml)
+	waypoints = gpx(dom).toJson()
+})
+
+
+
+var server = require('restify').createServer()
 
 function respond(request, response, next) {
 	response.send('Hello ' + (request.params.name || 'ambling rambler!'))
 }
-
-var server = restify.createServer()
 server.get('hello', respond)
 server.get('hello/:name', respond)
+
+
+server.get('waypoint/:index', waypoint)
+function waypoint(request, response, next) {
+	response.send(waypoints[request.params.index])
+}
+
 
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
