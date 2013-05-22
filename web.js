009be7717448aae5497988ac8896dd22f8b8b7ec
rajiv <rajiv.delwadia@versionone.com>
2013-05-21 22:01:00 -0400

    fixes

--- web.js
+++ web.js
@@ -1,14 +1,14 @@
 var restify = require('restify')
 
 function respond(request, response, next) {
-	response.send('Hello ' + request.params.name || 'ambling rambler!')
+	response.send('Hello ' + (request.params.name || 'ambling rambler!'))
 }
 
 var server = restify.createServer()
+server.get('hello', respond)
 server.get('hello/:name', respond)
-server.head('hello/:name', respond)
 
 var port = process.env.PORT || 5000;
 server.listen(port, function() {
-  console.log("%s listening on %s", server.name, server.url)
+	console.log("%s listening on %s", server.name, server.url)
 })
